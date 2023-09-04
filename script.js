e207e3f6844d48ee414e78266938c8cdfc849d64
vijaykrishna <vijaykrishna.top@gmail.com>
2023-09-05 00:19:09 +0530

    4th commit

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.addEventListener("DOMContentLoaded",()=>{
     
     const form=document.getElementById("form-1");
     form.addEventListener("submit",(e)=>{
-        e.pr
+        e.preventDefault();
         const location = document.querySelector(".location-input input").value;
       const checkIn = document.querySelector(".check-in input").value;
       const checkOut = document.querySelector(".check-out input").value;
       const guest = document.querySelector(".geust-no input").value;
-      localStorage.clear()
+      
       localStorage.setItem("Location",JSON.stringify(location));
       localStorage.setItem("CheckIn",checkIn);
       localStorage.setItem("CheckOut",checkOut);
